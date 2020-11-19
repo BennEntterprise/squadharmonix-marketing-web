efaf3df30e2d7a17e75f18619dbbfaa483dc9987
BennEntterprise <kyle@bennentterprise.com>
2020-11-19 20:21:43 +0000

    adjust server file to handle nested static builds

--- server.js
+++ server.js
@@ -2,19 +2,22 @@ const express = require('express')
 const favicon = require('express-favicon')
 const path = require('path')
 const port = process.env.PORT || 8080
+const host = process.env.HOST || 'localhost'
 const app = express()
 
-app.use(favicon(__dirname + '/build/favicon.ico'))
+app.use(favicon(__dirname + '/harmonix-client/build/favicon.ico'))
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname))
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, 'harmonix-client', 'build')))
 
 app.get('/ping', function (req, res) {
   return res.send('pong')
 })
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  res.sendFile(path.join(__dirname, 'harmonix-client', 'build', 'index.html'))
 })
-app.listen(port)
+app.listen(port, () => {
+  console.log(`Now Listening on: http://${host}:${port}`)
+})
