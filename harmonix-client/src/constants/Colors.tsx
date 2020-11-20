9a4af6558c5b9ca99cc52ae656f2cba412d776ee
BennEntterprise <kyle@bennentterprise.com>
2020-11-20 13:44:36 +0000

    add default color/font-family to a constants dir

---
+++ harmonix-client/src/constants/Colors.tsx
@@ -0,0 +1,13 @@
+// Pinks/Purples
+export const flower: string = '#fec5e9'
+export const magenta: string = '#fa4faa'
+export const purple: string = '#a800d6'
+
+//Blues
+export const tortise: string = '#99d5ca'
+export const teal: string = '#00c0c3'
+export const seagreen: string = '#01a589'
+
+//White/Black/Grey
+export const charcoal: string = '#202020'
+export const obsidian: string = '#090909'
