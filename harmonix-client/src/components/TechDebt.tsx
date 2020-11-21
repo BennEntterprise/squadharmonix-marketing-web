649d81bbdeee67a6b2e0c47156b1d6484f6526ce
BennEntterprise <kyle@bennentterprise.com>
2020-11-21 22:10:54 +0000

    add in a technical debt page, react-router

---
+++ harmonix-client/src/components/TechDebt.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const TechDebt = () => {
+  return (
+    <div>
+      <ol>
+        <li>
+          Replace Squad brand in nav bar with an actual logo on transparancy,
+          not a screen clipping (this will eliminate the box)
+        </li>
+        <li>
+          Move Nav bar items to the right, as of now they are up to the left
+          against the site brand.
+        </li>
+        <li>Buttons in the Groups (Squad and Harmonix) shood be same width.</li>
+        <li>
+          Find Better Assets for Socials (Need to be on transparant
+          backgrounds). Best if we can find a version directly from the
+          respective site's 'brand asset' pages.
+        </li>
+        <li>Get namecheap 'harmonics' redirected to 'harmonix'</li>
+        <li>Get namecheap to point at the heroku production deployment</li>
+      </ol>
+    </div>
+  )
+}
+
+export default TechDebt
