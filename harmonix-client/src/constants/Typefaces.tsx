b69566836c3b841a77c53d8aad46ba33b8f9367e
BennEntterprise <kyle@bennentterprise.com>
2020-11-28 17:42:21 +0000

    missed a typefacec

--- harmonix-client/src/constants/Typefaces.tsx
+++ harmonix-client/src/constants/Typefaces.tsx
@@ -4,15 +4,15 @@ import './Typefaces.css'
 const Typefaces = () => {
   return (
     <div>
-      <h1 className='jumbo'>Heading 1</h1>
-      <h1>Heading 1</h1>
-      <h2>Heading 2</h2>
-      <h3>Heading 3</h3>
-      <h4>Heading 4</h4>
-      <h5>Heading 5</h5>
-      <h6>Heading 6</h6>
-      <p>Paragraph</p>
-      <p className='mini'>p.mini</p>
+      <h1 className='font-jumbo'>Heading 1</h1>
+      <h1 className='font-h1'>Heading 1</h1>
+      <h2 className='font-h2'>Heading 2</h2>
+      <h3 className='font-h3'>Heading 3</h3>
+      <h4 className='font-h4'>Heading 4</h4>
+      <h5 className='font-h5'>Heading 5</h5>
+      <h6 className='font-h6'>Heading 6</h6>
+      <p className='font-h7'>Paragraph</p>
+      <p className='font-mini'>p.mini</p>
     </div>
   )
 }
