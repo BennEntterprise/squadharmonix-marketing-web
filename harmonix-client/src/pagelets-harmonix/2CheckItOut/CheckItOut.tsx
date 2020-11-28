43086c8728bef591703c770cb4e287a846432783
BennEntterprise <kyle@bennentterprise.com>
2020-11-28 23:38:32 +0000

    add titles to videos

--- harmonix-client/src/pagelets-harmonix/2CheckItOut/CheckItOut.tsx
+++ harmonix-client/src/pagelets-harmonix/2CheckItOut/CheckItOut.tsx
@@ -25,6 +25,7 @@ const CheckItOut = () => {
       <Row>
         <Col xs={12} md={12} lg={6} xl={6}>
           <div className='player-container'>
+            <h5 className='font-h5 liberation-sans-bold'>CALIFORNIA WINTER</h5>
             <ReactPlayer
               width='90%'
               heigth='90%'
@@ -35,6 +36,7 @@ const CheckItOut = () => {
         </Col>
         <Col xs={12} md={12} lg={6} xl={6}>
           <div className='player-container'>
+            <h5 className='font-h5 liberation-sans-bold'>PARTY IN THE USA</h5>
             <ReactPlayer
               width='90%'
               heigth='90%'
@@ -58,6 +60,9 @@ const CheckItOut = () => {
       <Row>
         <Col xs={12} md={12} lg={6} xl={6}>
           <div className='player-container'>
+            <h5 className='font-h5 liberation-sans-bold'>
+              SIMPLY THE BEST (FROM "SCHITTS CREEK")
+            </h5>
             <ReactPlayer
               width='90%'
               heigth='90%'
@@ -68,19 +73,38 @@ const CheckItOut = () => {
         </Col>{' '}
         <Col xs={12} md={12} lg={6} xl={6}>
           <div className='player-container'>
+            <h5 className='font-h5 liberation-sans-bold'>
+              STAND UP (FEAT. INTONENATION)
+            </h5>
             <ReactPlayer width='90%' heigth='90%' controls url={YTStandUp} />
           </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h3
+            id='quarantine-heading'
+            className='font-h3 color-teal liberation-sans-bold'
+          >
+            MAKEUP TUTORIALS
+          </h3>
         </Col>
       </Row>
 
       <Row>
         <Col xs={12} md={12} lg={6} xl={6}>
           <div className='player-container'>
+            <h5 className='font-h5 liberation-sans-bold'>
+              REINDEER LOOK - SYDNE ROSE
+            </h5>
             <ReactPlayer width='90%' heigth='90%' controls url={YTReindeer} />
           </div>
         </Col>{' '}
         <Col xs={12} md={12} lg={6} xl={6}>
           <div className='player-container'>
+            <h5 className='font-h5 liberation-sans-bold'>
+              ELF LOOK - STELLA LUCY
+            </h5>
             <ReactPlayer width='90%' heigth='90%' controls url={YTElf} />
           </div>
         </Col>
