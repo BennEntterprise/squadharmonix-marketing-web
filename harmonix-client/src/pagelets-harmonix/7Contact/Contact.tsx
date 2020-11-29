101b48168c1d92fd441247d468c1181f81257d5f
BennEntterprise <kyle@bennentterprise.com>
2020-11-29 20:55:10 +0000

    add high res pictures for contact

--- harmonix-client/src/pagelets-harmonix/7Contact/Contact.tsx
+++ harmonix-client/src/pagelets-harmonix/7Contact/Contact.tsx
@@ -3,12 +3,14 @@ import { Form, Button } from 'react-bootstrap'
 import './Contact.css'
 import axios from 'axios'
 import { flower } from '../../constants/Colors'
+
+import { Container, Row, Col } from 'react-bootstrap'
 //Import Images
-import ImgContact1 from '../../images/artist-contact/contact1.jpg'
-import ImgContact2 from '../../images/artist-contact/contact2.jpg'
-import ImgContact3 from '../../images/artist-contact/contact3.jpg'
-import ImgContact4 from '../../images/artist-contact/contact4.jpg'
-import ImgContact5 from '../../images/artist-contact/contact5.jpg'
+import ImgContact1 from '../../images/artist-contact/Sydne Contact.png'
+import ImgContact2 from '../../images/artist-contact/Simone Contact.png'
+import ImgContact3 from '../../images/artist-contact/Maya Contact.png'
+import ImgContact4 from '../../images/artist-contact/Lucy Contact.png'
+import ImgContact5 from '../../images/artist-contact/Lauren Contact.png'
 import HorizontalColorBand from '../../components/HorizontalColorBand'
 const Contact = () => {
   const [name, setName] = useState('')
@@ -65,67 +67,75 @@ const Contact = () => {
   }
 
   return (
-    <div id='contact-container'>
-      {/* <div id='contact-header-banner'>
-        <img src={ImgContact1} alt='artist' />
-        <img src={ImgContact2} alt='artist' />
-        <img src={ImgContact3} alt='artist' />
-        <img src={ImgContact4} alt='artist' />
-        <img src={ImgContact5} alt='artist' />
-      </div> */}
-      <HorizontalColorBand
-        color={flower}
-        height='50px'
-        style={{ marginBottom: '4rem' }}
-      />
-      <form id='artist-contact-form' onChange={(e: any) => handleFormChange(e)}>
-        <h1 className='font-juumbo  liberation-sans-bold'>CONTACT</h1>
-        <p className=' liberation-sans-bold'>
-          Let us know of any inquiries or requests here.
-        </p>
-        <input
-          className='form-input  liberation-sans-bold rounded'
-          placeholder='Name:'
-          name='name'
-          type='text'
-        />
-        <input
-          className='form-input  liberation-sans-bold rounded'
-          placeholder='Email:'
-          name='email'
-          type='email'
-        />
-        <input
-          className='form-input  liberation-sans-bold rounded'
-          placeholder='Subject:'
-          name='subject'
-          type='text'
-        />
-        <textarea
-          className='form-input  liberation-sans-bold rounded'
-          placeholder='Message:'
-          name='message'
-        ></textarea>
-        {sendError && (
-          <p style={{ color: 'red' }}>
-            OOPS There was an error sending your message. Please try again later
-          </p>
-        )}
-        {confirmation && (
-          <p style={{ color: 'green' }}>
-            You Message was sent successfully! We'll get in touch soon!
-          </p>
-        )}
-        <button
-          className='rounded liberation-sans-bold m-auto'
-          type='submit'
-          value='submit'
-          onClick={(e) => submitContactForm(e)}
-        >
-          SEND
-        </button>
-      </form>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <div id='contact-container'>
+            <div id='contact-header-banner'>
+              <img className='contact-image' src={ImgContact1} alt='artist' />
+              <img className='contact-image' src={ImgContact2} alt='artist' />
+              <img className='contact-image' src={ImgContact3} alt='artist' />
+              <img className='contact-image' src={ImgContact4} alt='artist' />
+              <img className='contact-image' src={ImgContact5} alt='artist' />
+            </div>
+            <form
+              id='artist-contact-form'
+              onChange={(e: any) => handleFormChange(e)}
+              style={{ width: '50%' }}
+            >
+              <h1 className='font-juumbo  liberation-sans-bold'>CONTACT</h1>
+              <p className=' liberation-sans-bold'>
+                Let us know of any inquiries or requests here.
+              </p>
+              <input
+                className='form-input  liberation-sans-bold rounded'
+                placeholder='Name:'
+                name='name'
+                type='text'
+              />
+              <input
+                className='form-input  liberation-sans-bold rounded'
+                placeholder='Email:'
+                name='email'
+                type='email'
+              />
+              <input
+                className='form-input  liberation-sans-bold rounded'
+                placeholder='Subject:'
+                name='subject'
+                type='text'
+              />
+              <textarea
+                className='form-input  liberation-sans-bold rounded'
+                placeholder='Message:'
+                name='message'
+                rows={4}
+                style={{ height: '8rem' }}
+              ></textarea>
+              {sendError && (
+                <p style={{ color: 'red' }}>
+                  OOPS There was an error sending your message. Please try again
+                  later
+                </p>
+              )}
+              {confirmation && (
+                <p style={{ color: 'green' }}>
+                  You Message was sent successfully! We'll get in touch soon!
+                </p>
+              )}
+              <button
+                className='rounded liberation-sans-bold m-auto'
+                type='submit'
+                value='submit'
+                onClick={(e) => submitContactForm(e)}
+              >
+                SEND
+              </button>
+            </form>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
