362228e11c742c0b29564989e7c43ebde2f80987
BennEntterprise <kyle@bennentterprise.com>
2020-12-02 22:29:41 +0000

    working to get button groups rigged up.

--- harmonix-client/src/pagelets-academy/1ThisIsSquad/ThisIsSquad.tsx
+++ harmonix-client/src/pagelets-academy/1ThisIsSquad/ThisIsSquad.tsx
@@ -72,7 +72,7 @@ const ThisIsSquad = () => {
         </Row>
       </Container>
       <Container fluid id='thisissquad'>
-        <Row id='flex-item-1'>
+        <Row>
           <Col
             className='squad-headline-container'
             xs={12}
@@ -93,61 +93,60 @@ const ThisIsSquad = () => {
             </div>
           </Col>
         </Row>
-        <Row
-          id='flex-item-2'
-          className='h-flex-container'
-          style={{ justifyContent: 'center' }}
-        >
-          <div
-            className='v-flex-container magenta-outline mr-5 ml-5'
-            id='btn-group-academy'
-          >
-            <h3>Squad&trade; Academy</h3>
-            <div id='squad-nav-btns' className='v-flex-container'>
-              <a href='#whatissquad'>
-                <button className='magenta-outline bg-grey'>
-                  WHAT IS SQUAD
-                </button>
-              </a>
-              <a href='#meettheteachers'>
-                <button className='magenta-outline bg-grey'>
-                  MEET THE TEACHERS
-                </button>
-              </a>
-              <a href='#meetthesquad'>
-                <button className='magenta-outline bg-grey'>
-                  MEET THE SQUAD
-                </button>
-              </a>
-              <a href='#toaudition'>
-                <button className='magenta-outline bg-grey'>AUDITION!</button>
-              </a>
+        <Row>
+          <Col xs='6' sm='6' md='6' lg='6' xl='6'>
+            <div
+              className='v-flex-container magenta-outline'
+              id='btn-group-academy'
+            >
+              <h3>Squad&trade; Academy</h3>
+              <div id='squad-nav-btns' className='v-flex-container'>
+                <a href='#whatissquad'>
+                  <button className='magenta-outline bg-grey'>
+                    WHAT IS SQUAD
+                  </button>
+                </a>
+                <a href='#meettheteachers'>
+                  <button className='magenta-outline bg-grey'>
+                    MEET THE TEACHERS
+                  </button>
+                </a>
+                <a href='#meetthesquad'>
+                  <button className='magenta-outline bg-grey'>
+                    MEET THE SQUAD
+                  </button>
+                </a>
+                <a href='#toaudition'>
+                  <button className='magenta-outline bg-grey'>AUDITION!</button>
+                </a>
+              </div>
             </div>
-          </div>
-
-          <div
-            className='v-flex-container teal-outline mr-5 ml-5 '
-            id='btn-group-harmonix'
-          >
-            <h3> Squad Harmonix&trade;</h3>
-            <div id='harmonix-nav-btns' className='v-flex-container'>
-              <a href='#music'>
-                <button className='teal-outline bg-grey'>MUSIC</button>
-              </a>
-              <a href='#about'>
-                <button className='teal-outline bg-grey'>ABOUT</button>
-              </a>
-              <a href=''>
-                <button className='teal-outline bg-grey'>MEMBERS</button>
-              </a>
-              <a href=''>
-                <button className='teal-outline bg-grey'>NEWS</button>
-              </a>
-              <a href=''>
-                <button className='teal-outline bg-grey'>MERCH</button>
-              </a>
+          </Col>
+          <Col xs='6' sm='6' md='6' lg='6' xl='6'>
+            <div
+              className='v-flex-container teal-outline  '
+              id='btn-group-harmonix'
+            >
+              <h3> Squad Harmonix&trade;</h3>
+              <div id='harmonix-nav-btns' className='v-flex-container'>
+                <a href='#music'>
+                  <button className='teal-outline bg-grey'>MUSIC</button>
+                </a>
+                <a href='#about'>
+                  <button className='teal-outline bg-grey'>ABOUT</button>
+                </a>
+                <a href=''>
+                  <button className='teal-outline bg-grey'>MEMBERS</button>
+                </a>
+                <a href=''>
+                  <button className='teal-outline bg-grey'>NEWS</button>
+                </a>
+                <a href=''>
+                  <button className='teal-outline bg-grey'>MERCH</button>
+                </a>
+              </div>
             </div>
-          </div>
+          </Col>
         </Row>
         <Row id='flex-item-3'>
           <Col>
