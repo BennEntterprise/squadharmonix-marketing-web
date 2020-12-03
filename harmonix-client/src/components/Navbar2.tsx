7f21a0f03b59f3f6d27dd04a9af837cfe74f2348
BennEntterprise <kyle@bennentterprise.com>
2020-12-03 13:43:42 +0000

    move nav items to the right on bar 2

--- harmonix-client/src/components/Navbar2.tsx
+++ harmonix-client/src/components/Navbar2.tsx
@@ -20,23 +20,32 @@ const Navbar2 = () => {
       }}
     >
       <Navbar.Brand href='#home'>What is Squad</Navbar.Brand>
-      <Nav className='mr-auto'>
-        <Nav.Link style={{ color: white }} href='#home'>
-          Home
-        </Nav.Link>
-        <Nav.Link style={{ color: white }} href='#'>
-          What is Squad?
-        </Nav.Link>
-        <Nav.Link style={{ color: white }} href='#'>
-          Meet The Squad
-        </Nav.Link>
-        <Nav.Link style={{ color: white }} href='#link'>
-          Meet The Teachers
-        </Nav.Link>
-        <Nav.Link style={{ color: white }} href='#'>
-          Audition
-        </Nav.Link>
-      </Nav>
+      <Navbar.Toggle className='navbar-dark' aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse>
+        <Nav
+          className='mr-auto float-right'
+          style={{
+            width: '100%',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <Nav.Link style={{ color: white }} href='#home'>
+            Home
+          </Nav.Link>
+          <Nav.Link style={{ color: white }} href='#'>
+            What is Squad?
+          </Nav.Link>
+          <Nav.Link style={{ color: white }} href='#'>
+            Meet The Squad
+          </Nav.Link>
+          <Nav.Link style={{ color: white }} href='#link'>
+            Meet The Teachers
+          </Nav.Link>
+          <Nav.Link style={{ color: white }} href='#'>
+            Audition
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   )
 }
