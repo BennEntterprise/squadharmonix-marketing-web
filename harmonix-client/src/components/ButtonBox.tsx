f7ca8957ceefb1e81bebb6f060085185a6cf0ba0
BennEntterprise <kyle@bennentterprise.com>
2020-12-10 13:56:16 +0000

    tidy the nav buttons in carousel

--- harmonix-client/src/components/ButtonBox.tsx
+++ harmonix-client/src/components/ButtonBox.tsx
@@ -7,8 +7,8 @@ const ButtonBox = () => {
     <Row>
       <Col xs='6' sm='6' md='6' lg='6' xl='6'>
         <div
-          className='v-flex-container magenta-outline'
           id='btn-group-academy'
+          className='v-flex-container btn-group-sq magenta-outline'
         >
           <h3 className='font-h3 liberation-sans-bold'>SQUAD ACADEMY</h3>
           <div
@@ -16,22 +16,22 @@ const ButtonBox = () => {
             className='v-flex-container box-navigation-buttons'
           >
             <a href='#whatissquad'>
-              <button className='magenta-outline bg-grey liberation-sans'>
+              <button className='magenta-outline bg-grey liberation-sans-bold rounded'>
                 WHAT IS SQUAD
               </button>
             </a>
             <a href='#meettheteachers'>
-              <button className='magenta-outline bg-grey liberation-sans'>
+              <button className='magenta-outline bg-grey liberation-sans-bold rounded'>
                 MEET THE TEACHERS
               </button>
             </a>
             <a href='#meetthesquad'>
-              <button className='magenta-outline bg-grey liberation-sans'>
+              <button className='magenta-outline bg-grey liberation-sans-bold rounded'>
                 MEET THE SQUAD
               </button>
             </a>
             <a href='#toaudition'>
-              <button className='magenta-outline bg-grey liberation-sans'>
+              <button className='magenta-outline bg-grey liberation-sans-bold rounded'>
                 AUDITION!
               </button>
             </a>
@@ -39,7 +39,10 @@ const ButtonBox = () => {
         </div>
       </Col>
       <Col xs='6' sm='6' md='6' lg='6' xl='6'>
-        <div className='v-flex-container teal-outline' id='btn-group-harmonix'>
+        <div
+          id='btn-group-harmonix'
+          className='v-flex-container btn-group-sq teal-outline'
+        >
           <h3 className='font-h3 liberation-sans-bold'>
             SQUAD HARMONIX&trade;
           </h3>
@@ -48,29 +51,29 @@ const ButtonBox = () => {
             className='v-flex-container box-navigation-buttons'
           >
             <a href='https://linktr.ee/squadharmonix'>
-              <button className='teal-outline bg-grey liberation-sans'>
+              <button className='teal-outline bg-grey liberation-sans-bold rounded'>
                 MUSIC
               </button>
             </a>
 
             <HashLink to='/harmonix#about-us-container'>
-              <button className='teal-outline bg-grey liberation-sans'>
+              <button className='teal-outline bg-grey liberation-sans-bold rounded'>
                 ABOUT
               </button>
             </HashLink>
 
             <HashLink to='/harmonix#members-container'>
-              <button className='teal-outline bg-grey  liberation-sans'>
+              <button className='teal-outline bg-grey  liberation-sans-bold rounded'>
                 MEMBERS
               </button>
             </HashLink>
             <HashLink to='/harmonix#press-container'>
-              <button className='teal-outline bg-grey  liberation-sans'>
+              <button className='teal-outline bg-grey  liberation-sans-bold rounded'>
                 NEWS
               </button>
             </HashLink>
             <HashLink to='/harmonix#merch-container'>
-              <button className='teal-outline bg-grey  liberation-sans'>
+              <button className='teal-outline bg-grey  liberation-sans-bold rounded'>
                 MERCH
               </button>
             </HashLink>
