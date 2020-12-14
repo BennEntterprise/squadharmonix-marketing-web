668e6d033fb60c75f50651895040e24eead27ddf
BennEntterprise <kyle@bennentterprise.com>
2020-12-14 14:05:28 +0000

    add stage lights/shadow to Senior Squad Video

--- harmonix-client/src/pagelets-academy/3MeetTheSquad/MeetTheSquad.tsx
+++ harmonix-client/src/pagelets-academy/3MeetTheSquad/MeetTheSquad.tsx
@@ -134,6 +134,9 @@ const MeetTheSquad = () => {
             xl='6'
             className='player-side'
           >
+            <div className='stage-light-graphic'>
+              <img src={ImgSpotlight} alt='spotlight' />
+            </div>
             <div className='academy-react-player'>
               <ReactPlayer
                 controls
@@ -141,6 +144,9 @@ const MeetTheSquad = () => {
                 width='85%'
                 height='100%'
               />
+            </div>
+            <div className='stage-shadow-graphic'>
+              <img src={ImgWhiteShadow} alt='spotlight' />
             </div>
           </Col>
           <Col xs='12' sm='12' md='12' lg='6' xl='6' className='gallery-side'>
