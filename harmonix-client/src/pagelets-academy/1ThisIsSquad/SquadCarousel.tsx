250ce6045580d5b29c806220d4bb7637094bd41e
BennEntterprise <kyle@bennentterprise.com>
2020-12-21 16:34:46 +0000

    Extract Carousel to a separate component

---
+++ harmonix-client/src/pagelets-academy/1ThisIsSquad/SquadCarousel.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import { Carousel } from 'react-bootstrap'
+const SquadCarousel = () => {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <div id='carousel-img-1' className='carousel-img'></div>
+        <CarouselBannerCopy
+          heading={'THIS IS SQUAD.'}
+          subHeading={
+            'an inspiring learning community for dedicated vocalists agess 8-17'
+          }
+          tidbit={'ONLINE SINCE MAY!'}
+          textAlignment='left'
+        />
+        <Carousel.Caption className='carousel-caption-a'></Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div id='carousel-img-2' className='carousel-img'></div>
+        <CarouselBannerCopy
+          heading={'Find your passion.'}
+          subHeading='Shape your musical experience with our many A Cappella training programs, from voice to music theory to beatboxing.'
+          tidbit='ONLINE SINCE MAY!'
+          textAlignment='right'
+        />
+        <Carousel.Caption className='carousel-caption-a'></Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div id='carousel-img-3' className='carousel-img'></div>
+        <CarouselBannerCopy
+          heading='Share the screen.'
+          subHeading='Take part in collaborative projects that bring us back together in an otherwise distant world.'
+          tidbit='ONLINE SINCE MAY!'
+          textAlignment='left'
+        />
+        <Carousel.Caption className='carousel-caption-a'></Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  )
+}
+
+interface ICarouselBannerCopy {
+  heading?: string
+  subHeading?: string
+  tidbit?: string
+  textAlignment?: 'left' | 'right' | 'center'
+}
+const CarouselBannerCopy = (props: ICarouselBannerCopy) => {
+  let floatVal: 'left' | 'right' | 'none'
+  switch (props.textAlignment) {
+    case 'left':
+      floatVal = 'left'
+      break
+    case 'right':
+      floatVal = 'right'
+      break
+    default:
+      floatVal = 'none'
+      break
+  }
+
+  return (
+    <div className='carousel-banner-copy'>
+      <h1 style={{ textAlign: props.textAlignment }}>{props.heading}</h1>
+      <h6 style={{ textAlign: props.textAlignment }}>{props.subHeading}</h6>
+      {props.tidbit && (
+        <p
+          style={{
+            textAlign: props.textAlignment,
+            zIndex: 1,
+            float: floatVal,
+          }}
+        >
+          {props.tidbit}
+        </p>
+      )}
+    </div>
+  )
+}
+
+export default SquadCarousel
--- harmonix-client/src/pagelets-academy/1ThisIsSquad/ThisIsSquad.tsx
+++ harmonix-client/src/pagelets-academy/1ThisIsSquad/ThisIsSquad.tsx
@@ -26,44 +26,7 @@ import SpLogo from '../../images/icons/spotify-academy.png'
 import TtLogo from '../../images/icons/tiktok-academy.png'
 import FbLogo from '../../images/icons/facebook-academy.png'
 
-interface ICarouselBannerCopy {
-  heading?: string
-  subHeading?: string
-  tidbit?: string
-  textAlignment?: 'left' | 'right' | 'center'
-}
-const CarouselBannerCopy = (props: ICarouselBannerCopy) => {
-  let floatVal: 'left' | 'right' | 'none'
-  switch (props.textAlignment) {
-    case 'left':
-      floatVal = 'left'
-      break
-    case 'right':
-      floatVal = 'right'
-      break
-    default:
-      floatVal = 'none'
-      break
-  }
-
-  return (
-    <div className='carousel-banner-copy'>
-      <h1 style={{ textAlign: props.textAlignment }}>{props.heading}</h1>
-      <h6 style={{ textAlign: props.textAlignment }}>{props.subHeading}</h6>
-      {props.tidbit && (
-        <p
-          style={{
-            textAlign: props.textAlignment,
-            zIndex: 1,
-            float: floatVal,
-          }}
-        >
-          {props.tidbit}
-        </p>
-      )}
-    </div>
-  )
-}
+import SquadCarousel from './SquadCarousel'
 
 const ThisIsSquad = () => {
   const imgHeight: string = '50px'
@@ -81,40 +44,7 @@ const ThisIsSquad = () => {
             xl='12'
             style={{ paddingLeft: '0%', paddingRight: '0px' }}
           >
-            <Carousel>
-              <Carousel.Item>
-                <div id='carousel-img-1' className='carousel-img'></div>
-                <CarouselBannerCopy
-                  heading={'THIS IS SQUAD.'}
-                  subHeading={
-                    'an inspiring learning community for dedicated vocalists agess 8-17'
-                  }
-                  tidbit={'ONLINE SINCE MAY!'}
-                  textAlignment='left'
-                />
-                <Carousel.Caption className='carousel-caption-a'></Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <div id='carousel-img-2' className='carousel-img'></div>
-                <CarouselBannerCopy
-                  heading={'Find your passion.'}
-                  subHeading='Shape your musical experience with our many A Cappella training programs, from voice to music theory to beatboxing.'
-                  tidbit='ONLINE SINCE MAY!'
-                  textAlignment='right'
-                />
-                <Carousel.Caption className='carousel-caption-a'></Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <div id='carousel-img-3' className='carousel-img'></div>
-                <CarouselBannerCopy
-                  heading='Share the screen.'
-                  subHeading='Take part in collaborative projects that bring us back together in an otherwise distant world.'
-                  tidbit='ONLINE SINCE MAY!'
-                  textAlignment='left'
-                />
-                <Carousel.Caption className='carousel-caption-a'></Carousel.Caption>
-              </Carousel.Item>
-            </Carousel>
+            <SquadCarousel />
           </Col>
         </Row>
         <Row>
